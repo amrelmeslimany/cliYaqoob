456795195f07264b7e2ca88e31b971778d45f0cc
amrelmeslimany <66785979+amrelmeslimany@users.noreply.github.com>
2021-04-23 01:14:49 +0200

    Upload 23-4  N-1

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  /* 
+        !Road Map
+    * 1 ) Navbar
+    * 2 ) Main
+    * 3 ) Exploring
+    * 4 ) About Us
+    * 5 ) Servics
+    * 6 ) Contact Us
+    * 7 ) Footer
+*/
+});
